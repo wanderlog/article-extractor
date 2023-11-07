797c7d4bb75ee60724ee12b3aa8699b10ba62aef
Peter Xu <px.peter.xu@gmail.com>
2023-11-06 16:20:54 -0800

    Patch extractus to use same Readability configuration (clean fewer things) that we use

--- src/utils/extractWithReadability.js
+++ src/utils/extractWithReadability.js
@@ -15,6 +15,21 @@ export default (html, url = '') => {
   const reader = new Readability(doc, {
     keepClasses: true,
   })
+
+  // Strip out as little as possible; otherwise, Readability will
+  // try to remove elements with class "clearfix", which might remove
+  // some real content divs
+  reader._flags = 0
+  // Typically, _clean will do a bunch of after-parse cleanup
+  // by e.g., removing <h1> tags. This wreaks havoc with sites like Eater,
+  // which puts restaurant names in <h1> tags, so we disable it for
+  // certain tags
+  const origClean = reader._clean.bind(reader)
+  reader._clean = (content, tagName) => {
+    if (tagName === 'h1') return
+    origClean(content, tagName)
+  }
+
   const result = reader.parse() ?? {}
   return result.textContent ? result.content : null
 }
